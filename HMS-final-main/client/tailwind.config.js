a6feb0202d8e68645a535a0a34c06d30b914a496
saideepvayyala <saideepvayyala@gmail.com>
2024-08-20 19:41:54 +0530

    first commit

---
+++ HMS-final-main/client/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F25555",
+        secondary: "#9CB2AA",
+        tertiary: "#B2D8CB",
+        bgprimary: "#FAFCFF",
+        bgsecondary: "#fcdadb",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "ui-sans-serif"],
+        poppins: ["Poppins", "ui-sans-serif"],
+        lato: ["Lato", "SFMono-ui-monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
